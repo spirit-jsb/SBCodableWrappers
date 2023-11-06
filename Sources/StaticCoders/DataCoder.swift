4fd6bf17708f2910640dca1dcafd772f6af23d47
Max <sibo_jian_29903549@163.com>
2023-11-06 13:52:05 +0800

    refactor: Optimize code

--- Sources/StaticCoders/DataCoder.swift
+++ Sources/StaticCoders/DataCoder.swift
@@ -13,8 +13,8 @@ public struct Base64DataStaticCoder: StaticCoder {
     public static func decode(from decoder: Decoder) throws -> Data {
         let stringValue = try String(from: decoder)
 
-        if let base64Data = Data(base64Encoded: stringValue) {
-            return base64Data
+        if let data = Data(base64Encoded: stringValue) {
+            return data
         } else {
             throw DecodingError.valueNotFound(self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "expected \(Data.self) but could not convert \(stringValue) to Data"))
         }
